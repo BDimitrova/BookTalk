17aecea60dfce3d0ab654769b11571b8ed38897c
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-07-19 15:11:20 +0300

    Setup handlebars

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+require('./config/hbsConfig')(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
-    res.end();
+    res.render('home');
 });
 
 app.listen(port, () => console.log(`The app is running on http://localhost:${port}`));
