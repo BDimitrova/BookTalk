3554984f34d67daaf38e2b654b660a659355f3ac
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-07-22 11:41:04 +0300

    Connecting catalog, profile and create page

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,4 +4,16 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
+router.get('/profile', (req, res) => {
+    res.render('profile');
+});
+
+router.get('/catalog', (req, res) => {
+    res.render('catalog');
+});
+
+router.get('/create-review', (req, res) => {
+    res.render('create');
+});
+
 module.exports = router;
